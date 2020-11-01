dc3fe3e7e6ee9eb8cac060568519bf929da81754
BAUHAUS <patrickfromgnomedk@gmail.com>
2020-11-01 21:52:01 +0100

    Added Placement
    Need fixes

--- Assets/Scripts/MouseScript.cs
+++ Assets/Scripts/MouseScript.cs
@@ -9,7 +9,7 @@ public class MouseScript : MonoBehaviour
     [SerializeField] float cellSize = 1f;
     [SerializeField] int width = 8, height = 5;
     [SerializeField] Vector3 originPos = new Vector3(-3,-5);
-
+    public GameObject currentPlacement;
 
     // Start is called before the first frame update
     void Start()
@@ -31,16 +31,20 @@ public class MouseScript : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-        
+
         //primary click
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             Vector2Int MousePos = world.GetCell(TGrid<int>.GetMouseWorldPos());
-            Debug.Log($"X = {MousePos.x}, Y = {MousePos.y}, VALUE : {world.gridArray[MousePos.x,MousePos.y]}");
+            Debug.Log($"X = {MousePos.x}, Y = {MousePos.y}, VALUE : {world.gridArray[MousePos.x, MousePos.y]}");
+            GameObject placement = Instantiate(currentPlacement, transform);
+            // Not sure why it's not going to the right position, I asked people on Unity Discord but
+            // I didn't understand what they meant
+            placement.transform.position = world.GetWorldPosition(MousePos.x , MousePos.y);
         }
 
         //secondary click
-        if(Input.GetMouseButtonDown(1)){
+        if (Input.GetMouseButtonDown(1)){
             int value = 26;
             world.SetValue(TGrid<int>.GetMouseWorldPos(), value);
         }
