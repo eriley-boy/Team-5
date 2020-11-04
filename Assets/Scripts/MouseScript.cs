2ad5004998a42285b633dc1289943d2f0d81b755
eriley-boy <57048063+eriley-boy@users.noreply.github.com>
2020-11-04 09:46:59 -0500

    Update MouseScript.cs

--- Assets/Scripts/MouseScript.cs
+++ Assets/Scripts/MouseScript.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI
 
 public class MouseScript : MonoBehaviour
 {
+    //amt of mats
+    public float mats;
+    //how many mats are being used when building
+    public float matamtused;
+    public text matcounter;
     public TGrid<int> world;
     [SerializeField] float cellSize = 1f;
     [SerializeField] int width = 8, height = 5;
@@ -38,9 +44,18 @@ public class MouseScript : MonoBehaviour
         //primary click
         if (Input.GetMouseButtonDown(0))
         {
+        //i made some changes @ me if there is a problem
+            if(!(mats -= matamtused = <= 0))
+            {
             Vector2Int MousePos = world.GetCell(TGrid<int>.GetMouseWorldPos());
             Debug.Log($"X = {MousePos.x}, Y = {MousePos.y}, VALUE : {world.gridArray[MousePos.x, MousePos.y]}");
             highlightMap.SetTile((Vector3Int)MousePos, highlightTile);
+            mats - matamtused;
+            matcounter.text = mats;
+            } else
+            {
+                Debug.log(¨you only have ¨ + mats¨ + ¨. Get more to build¨);
+            }
 
         }
 
